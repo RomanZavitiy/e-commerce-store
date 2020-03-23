94f0ffb65cb5f495b2f53c05429d0aa561d42dc5
RomanZavitiy <zavitiy95@gmail.com>
2020-03-23 23:23:20 +0100

    Refactored user Schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
-var mongoose = require("mongoose");
+var mongoose = require ('mongoose');
+    Schema   = mongoose.Schema;
+    bcrypt   = require ('bcrypt-nodejs');
 
-var UserSchema = new mongoose.Schema({
+
+var UserSchema = new Schema({
     username: {type: String, unique: true, required: true},
     password: String,
     firstName: String,
@@ -11,5 +14,13 @@ var UserSchema = new mongoose.Schema({
     resetPasswordExpires: Date,
     isAdmin: {type: Boolean, default: false}
 });
+
+user.Schema.methods.enryptPassword = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+}
+
+user.Schema.methods.validPassword = function(password){
+    return bcrypt.compare(password, this.password);
+}
 
 module.exports = mongoose.model("User", UserSchema);
