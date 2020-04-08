c77efdd5a8cf5a51efae012298e37df88be2d0d3
RomanZavitiy <zavitiy95@gmail.com>
2020-04-08 15:01:30 +0200

    Implement function for success client charging with Stripe.js

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,20 @@ var express = require('express');
     Cart    = require('../models/cart')
     
 
-/* GET home page. */
+// HOME PAGE WITH PRODUCTS
 router.get('/', function(req, res) {
   // Get all campgrounds from DB
+  var successMsg = req.flash('success')[0];
   Product.find({}, function(err, allProducts){
     if(err){
         console.log(err);
     } else {
-        res.render("index",{products: allProducts});
+        res.render("index", {products: allProducts, successMsg: successMsg, noMessages: !successMsg});
     }
     });
 });
 
+// ADD TO CART FUNCTION
 router.get('/add-to-card/:id', function(req, res, next){
     var productId = req.params.id;
     var cart = new Cart(req.session.cart ? req.session.cart : {} );
@@ -32,6 +34,7 @@ router.get('/add-to-card/:id', function(req, res, next){
     });
 });
 
+// SHOW SHOPPING CART
 router.get('/shopping-cart', function(req, res, next){
     if(!req.session.cart) {
         return res.render('shop/shopping-cart', {products: null});
@@ -40,12 +43,44 @@ router.get('/shopping-cart', function(req, res, next){
         res.render('shop/shopping-cart', {products: cart.generateArray(), totalQty: cart.totalQty, totalPrice: cart.totalPrice});
 });
 
+// SHOW CHECKOUT PAGE
 router.get('/checkout', function(req, res, next){
     if(!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
+    var errMsg = req.flash('error')[0];
     var cart = new Cart(req.session.cart);
-    res.render('shop/checkout', {total: cart.totalPrice});
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+// CHARGING A CLIENT
+router.post('/checkout', function(req, res, next){
+    if(!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    
+    var cart = new Cart(req.session.cart);
+
+    var stripe = require('stripe')(
+        'sk_test_TT9pzsGcMtTBzBlr7bCVTOEh001FX82p1O'
+    );
+
+    stripe.charges.create(
+    {
+        amount: cart.totalPrice * 100, // amount in grosz
+        currency: 'pln',
+        source: req.body.stripeToken,
+        description: 'Test charge.', //obtained with Stripe.js
+    },
+    function(err, charge) {
+        if(err){
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+        req.flash('success', 'Test purchase made with success');
+        req.session.cart = null;
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
